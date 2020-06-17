a32ea188c5a1abc1acae09132ce2c3ce6df620be
Crowdbotics <team@crowdbotics.com>
2020-06-17 01:35:06 +0000

    Update models

--- backend/dreamr_18156/wsgi.py
+++ backend/dreamr_18156/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dreamr_18156.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dreamr_18156.settings")
 
 application = get_wsgi_application()
